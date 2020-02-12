556efd6f5dbc6535a029c8127c15502a2dcf07c6
Waseem Ahmed <waseema@bbd.co.za>
2020-02-12 16:15:13 +0200

    basic method

---
+++ methods/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static int MyMethod(int a){
+                int result = a * 2;
+                return result;
+            }
+            Console.WriteLine(MyMethod(2));
+        }
+    }
+}
