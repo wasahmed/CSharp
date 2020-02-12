3702a2baae47187d4da9aab581c798635a9b67e4
Waseem Ahmed <waseema@bbd.co.za>
2020-02-12 11:20:59 +0200

    C# Math

---
+++ numbers/numbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace numbers
+{
+    class numbers
+    {
+        static void Main(string[] args)
+        {
+            //some Math methods
+            Console.WriteLine(Math.Abs(-10));
+            Console.WriteLine(Math.Pow(2, 2));
+            Console.WriteLine(Math.Max(4, 10));
+            Console.WriteLine(Math.Round(4.6));
+        }
+    }
+}
