2c5f00fd081c624af2fcc679e665aa0e3c914cff
Waseem Ahmed <waseema@bbd.co.za>
2020-02-12 11:42:31 +0200

    taking in user input

---
+++ input/Input.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace input
+{
+    class Input
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter name");
+            string name =Console.ReadLine();
+            Console.WriteLine("Enter your age");
+            string age = Console.ReadLine();
+            Console.WriteLine("your name is " + name +" and your age is " + age);
+        }
+    }
+}
