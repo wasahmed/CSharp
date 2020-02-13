19092c007e853ed72dd745921f99f5a247672b49
Waseem Ahmed <waseema@bbd.co.za>
2020-02-13 14:49:49 +0200

    can not be instantiated

---
+++ static_class/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace static_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            staticc x = new staticc();
+            Console.WriteLine(x.color);
+        }
+    }
+}
---
+++ static_class/static.cs
@@ -0,0 +1,7 @@
+
+namespace static_class{
+
+    public static class staticc{
+        public string color = "red";
+    }
+}
